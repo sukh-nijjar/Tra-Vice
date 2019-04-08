040b9102ea21f6cdcbe0888a295b6a00c4a2951c
sukh-nijjar <sukhbinder.nijjar@googlemail.com>
2019-04-09 00:03:01 +0100

    initial push from local

---
+++ main.py
@@ -0,0 +1,92 @@
+import os
+import urllib.request as req
+from flask import Flask, render_template, request, jsonify, json
+
+# lines list is used to generate station names
+lines = ["bakerloo","central","circle","district","hammersmith-city","jubilee",
+         "metropolitan","northern","piccadilly","victoria","waterloo-city",
+         "tfl-rail","london-overground"]
+
+# brand_colours dict is used to pass correct line branding to views
+brand_colours = {"bakerloo" : "#B36305", "central" : "#E32017", "circle" : "#FFD300",
+                 "district" : "#00782A", "hammersmith-city" : "#F3A9BB", "jubilee" : "#A0A5A9",
+                 "metropolitan" : "#9B0056", "london-overground" : "#e86a10", "northern" : "#000000",
+                 "piccadilly" : "#003688", "tfl-rail" : "#0019a8", "victoria" : "#0098D4","waterloo-city" : "#95CDBA"}
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    # stations = get_all_stations()
+    return render_template("index.html", colours = brand_colours)
+
+@app.route("/arrivals", methods=["GET"])
+def get_arrivals_for_station():
+    print("/arrivals invoked")
+    # print("STATION NAME {}, LINE {}".format(request.args.get("station"), request.args.get("line")))
+    station_id = get_station_id(request.args.get("station"))
+    print("...", station_id)
+    line = request.args.get("line")
+    # print("STATION ID {}, LINE {}".format(station_id, line))
+    url = "https://api.tfl.gov.uk/Line/" + line + "/Arrivals/" + station_id
+    print("url", url)
+    try:
+        arrivals = call_TFL_API(url)
+        print("type(arrivals)")
+        station_name = arrivals[0]["stationName"]
+        return jsonify({"done" : arrivals,
+                        "station" : station_name})
+    except:
+        message = "Unable to connect to TfL's API at the moment. Route = /arrivals."
+        return jsonify({"error_msg" : message})
+
+@app.route("/stations", methods=["GET"])
+def get_stations():
+    line = request.args.get("line")
+    url = "https://api.tfl.gov.uk/Line/" + line + "/StopPoints"
+    try:
+        line_stations = call_TFL_API(url)
+        return jsonify({"done" : line_stations})
+    except:
+        message = "Unable to connect to TfL's API at the moment. Route = /stations."
+        return jsonify({"error_msg" : message})
+
+def get_station_id(station_name):
+    stops = station_data()
+    for stop in stops:
+        if stop["station_name"] == station_name:
+            station_id = stop["station_id"]
+    return station_id
+
+# def get_stations_for_line(line):
+#     stops = station_data()
+#     stops_for_line = [stop for stop in stops if stop["line_id"] == line]
+#     return stops_for_line
+
+# def get_all_stations():
+#     '''gets ALL stations'''
+#     tube_stops_list = list()
+#     tube_stops = station_data()
+#     for stop in tube_stops:
+#         #without this check station names get repeated
+#         if tube_stops_list.count(stop["station_name"]) == 0:
+#             tube_stops_list.append(stop["station_name"])
+#     return tube_stops_list
+#
+def station_data():
+    if os.path.isfile("all_stations.json"):
+        with open("all_stations.json","r") as stations:
+            return json.load(stations)
+
+def call_TFL_API(url_in):
+    credentials = "?app_id=86e5ac12&app_key=%206527cf5c3e54e1fefd6fa890a6cfa75b"
+    url = url_in + credentials
+    conn = req.urlopen(url)
+    data = conn.read()
+    str_data = data.decode("utf8")
+    API_response = json.loads(str_data)
+    return API_response
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ static/scripts/main.js
@@ -0,0 +1,236 @@
+/*
+	Overflow by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+(function($) {
+
+	var	$window = $(window),
+		$body = $('body'),
+		settings = {
+
+			// Parallax background effect?
+				parallax: true,
+
+			// Parallax factor (lower = more intense, higher = less intense).
+				parallaxFactor: 10
+
+		};
+
+	// Breakpoints.
+		breakpoints({
+			wide:    [ '1081px',  '1680px' ],
+			normal:  [ '841px',   '1080px' ],
+			narrow:  [ '737px',   '840px'  ],
+			mobile:  [ null,      '736px'  ]
+		});
+
+	// Mobile?
+		if (browser.mobile)
+			$body.addClass('is-scroll');
+
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+
+	// Scrolly.
+		$('.scrolly-middle').scrolly({
+			speed: 1000,
+			anchor: 'middle'
+		});
+
+		$('.scrolly').scrolly({
+			speed: 1000,
+			offset: function() { return (breakpoints.active('<=mobile') ? 70 : 190); }
+		});
+
+	// Parallax background.
+
+		// Disable parallax on IE/Edge (smooth scrolling is jerky), and on mobile platforms (= better performance).
+			if (browser.name == 'ie'
+			||	browser.name == 'edge'
+			||	browser.mobile)
+				settings.parallax = false;
+
+		if (settings.parallax) {
+
+			var $dummy = $(), $bg;
+
+			$window
+				.on('scroll.overflow_parallax', function() {
+
+					// Adjust background position.
+						$bg.css('background-position', 'center ' + (-1 * (parseInt($window.scrollTop()) / settings.parallaxFactor)) + 'px');
+
+				})
+				.on('resize.overflow_parallax', function() {
+
+					// If we're in a situation where we need to temporarily disable parallax, do so.
+						if (breakpoints.active('<=narrow')) {
+
+							$body.css('background-position', '');
+							$bg = $dummy;
+
+						}
+
+					// Otherwise, continue as normal.
+						else
+							$bg = $body;
+
+					// Trigger scroll handler.
+						$window.triggerHandler('scroll.overflow_parallax');
+
+				})
+				.trigger('resize.overflow_parallax');
+
+		}
+
+	// Poptrox.
+		$('.gallery').poptrox({
+			useBodyOverflow: false,
+			usePopupEasyClose: false,
+			overlayColor: '#0a1919',
+			overlayOpacity: 0.75,
+			usePopupDefaultStyling: false,
+			usePopupCaption: true,
+			popupLoaderText: '',
+			windowMargin: 10,
+			usePopupNav: true
+		});
+
+		// these 2 are used to scroll the screen along
+		$("#scroll_on_submit, #scroll_to_stations_for_line").hide();
+
+		$(".line_button").on("click", function(){
+			let line_selected = $(this).text();
+			$.ajax({
+				data : {
+					line : line_selected
+				},
+				type : "GET",
+				url : "/stations"
+			})
+			.done(function(data){
+				// associate *SET* the line with the unordered list which contains dynamically created li elements (1 per station)
+				// this stores data against the element but does not affect the DOM
+				// don't confuse this data function with the data being processed by .done()!
+				$("#stations_list").data("line_name", line_selected);
+
+				const stations = [];
+				let stringToRemove;
+				// strip out superfluous text from station names returned by TfL API
+				$.each(data.done, function(i,item){
+					if (item.commonName.includes("Underground")) {
+						stringToRemove = "Underground Station";
+					} else {
+						stringToRemove = "Rail Station";
+					};
+					shortenedStationName = removeTextFromString(item.commonName,stringToRemove);
+					stations.push(shortenedStationName);
+				});
+
+				// clear out arrivals information from previous request
+				if ($("#arrivals_rows").length) {
+					clearArrivalsTable();
+				}
+				// clear out existing radio buttons from previous request
+				$("#stations_list").empty();
+				// display line name for the stations being displayed
+				$(".stations_for_line").text(line_selected + " line stations");
+				// build new set of radio buttons
+				stations.forEach(buildStationsCollection);
+				// hide the get trains link
+				$("div.get_trains").hide();
+				$("#scroll_to_stations_for_line").trigger("click");
+				
+			})//end .done function
+			.fail(function(data) {
+				// replace this by scrolling to a error panel
+				console.log(data.error_msg);
+			})
+		});
+
+		$("ul").delegate("li label","click", function() {
+			// hide any divs (containing 'get trains' anchor tag)
+			// made visible each time a label is clicked
+			$("div.get_trains:visible").each(function() {
+				$(this).hide();
+			})
+			// make visible the div containing 'get trains' anchor tag
+			$(this).nextAll("div.get_trains").show();
+		})
+
+		// CODE TO INVOKE AJAX CALL TO '/arrivals'
+		$("ul").delegate(".arrivals_link", "click", function() {
+			// let station = $(this).closest("ul li").find("label").text();
+			// get params required for calling arrivals API
+			let station_name = $(this)
+			.closest("ul li")
+			.find("input").val();
+			// retrieve *GET* the line from the data stored against #stations_list
+			let line_selected = $("#stations_list").data("line_name");
+			console.log("LINE SELECTED ", line_selected);
+			$.ajax({
+				data: {
+					station : station_name,
+					line : line_selected
+				},
+				type : "GET",
+				url : "/arrivals"
+			})
+			.done(function(data) {
+				if (data.done) {
+					// clear out arrivals information from previous request
+					if ($("#arrivals_rows").length) {
+						clearArrivalsTable();
+					}
+					$("#arrivals_table").append("<thead><tr><th>Destination</th>" +
+														 "<th>Expected</th>" +
+														 "<th>Current Location</th></tr></thead>");
+
+					$.each(data.done, function(i,item) {
+						buildArrivalsTable(item);
+						// console.log("NaptanID ", item.naptanId);
+					})
+					$('#arrivals_station').append(data.station);
+				} else {
+					console.log("need to implement no arrivals use case");
+				}
+			})// end done
+			.fail(function(data) {
+				console.log(data.error_msg);
+			})
+		})
+
+		function removeTextFromString(str, textToStrip) {
+			const result = str.replace(textToStrip,"").trim();
+			return result;
+		}
+		//
+		function buildStationsCollection(element) {
+			let input_element = "<input id='" + element + "' type='radio' name='selected_station' value='" + element + "'>";
+			let label_elemet = "<label for='" + element + "'>" + element + "</label>";
+			let check_div = "<div class='check'></div>";
+			let get_trains_div = "<div class='get_trains'><a href='#banner' class='arrivals_link'>Get Trains</a></div>";
+			$("#stations_list").append(`<li>` + input_element + label_elemet + check_div + get_trains_div + `</li>`);
+		}
+
+		function buildArrivalsTable(element) {
+			let table_row = "<tr><td>" + element.destinationName + "</td>" +
+											"<td>" + new Date(element.expectedArrival) + "</td>" +
+											"<td>" + element.currentLocation + "</td></tr>"
+
+			$('#arrivals_rows').append(table_row);
+		}
+
+		function clearArrivalsTable() {
+			$('#arrivals_station').empty();
+			$("#arrivals_rows").empty();
+			$("thead").remove();
+		}
+
+})(jQuery);
